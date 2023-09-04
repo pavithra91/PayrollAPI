8f309df3f463895fc02159f78bcfecf8eafcb88d
pavithra91 <dev.pavithraj@gmail.com>
2023-09-04 15:03:54 +0530

    CPP-24 Host Application on Server and add relevent controllers and classes.

---
+++ PayrollAPI/Controllers/DataController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PayrollAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataController : ControllerBase
+    {
+    }
+}
---
+++ PayrollAPI/Controllers/PayrollController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PayrollAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PayrollController : ControllerBase
+    {
+        public PayrollController() { }
+    }
+}
