f1238970b8008656c92e0e2c878ced54e553730f
pavithra91 <dev.pavithraj@gmail.com>
2023-09-11 06:43:06 +0530

    CPP-29 add CalDto class

---
+++ PayrollAPI/DataModel/CalDto.cs
@@ -0,0 +1,20 @@
+﻿namespace PayrollAPI.DataModel
+{
+    public class CalDto
+    {
+        public int id { get; set; }
+        public char flag { get; set; }
+        public int companyCode { get; set; }
+        public int sequence { get; set; }
+        public int payCode { get; set; }
+        public string? calCode { get; set; }
+        public string? payCategory { get; set; }
+        public string? calFormula { get; set; }
+        public string? calDescription { get; set; }
+        public bool status { get; set; }
+        public string? createdBy { get; set; }
+        public DateTime createdDate { get; set; }
+        public string? lastUpdateBy { get; set; }
+        public DateTime lastUpdateDate { get; set; }
+    }
+}
