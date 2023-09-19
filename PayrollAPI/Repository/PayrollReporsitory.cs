a1c6e2d6e2ba7d013e8ec6b2dad607c877956510
pavithra91 <dev.pavithraj@gmail.com>
2023-09-19 16:30:25 +0530

    CPP-29 Clean Data before Payrun

--- PayrollAPI/Repository/PayrollReporsitory.cs
+++ PayrollAPI/Repository/PayrollReporsitory.cs
@@ -1,27 +1,64 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
 using PayrollAPI.Data;
 using PayrollAPI.DataModel;
 using PayrollAPI.Interfaces;
 using PayrollAPI.Models;
+using System.Data;
+using System.Reflection.Metadata.Ecma335;
 
 namespace PayrollAPI.Repository
 {
     public class PayrollReporsitory : IPayroll
     {
-        private readonly DBConnect _dbConnect;
+        private readonly DBConnect _context;
 
         public PayrollReporsitory(DBConnect db)
         {
-            _dbConnect = db;
+            _context = db;
         }
 
         public void ProcessPayroll(ApprovalDto approvalDto)
         {
-            ICollection<Employee_Data> _emp = _dbConnect.Employee_Data.Where(o => o.period == approvalDto.period).ToList();
-            ICollection<Payroll_Data> _payrollData = _dbConnect.Payroll_Data.Where(o => o.period == approvalDto.period).ToList();           
-            ICollection<EmpSpecialRate> _empSpecialRates = _dbConnect.EmpSpecialRate.Where(o => o.status == true).ToList();
+            ICollection<Employee_Data> _emp = _context.Employee_Data.Where(o => o.period == approvalDto.period).ToList();
+            ICollection<Payroll_Data> _payrollData = _context.Payroll_Data.Where(o => o.period == approvalDto.period).ToList();
+            ICollection<EmpSpecialRate> _empSpecialRates = _context.EmpSpecialRate.Where(o => o.status == true).ToList();
 
+           // using var transaction = BeginTransaction();
 
+            Parallel.ForEach(_empSpecialRates, payItem =>
+            {
+                Payroll_Data _data = new Payroll_Data();
+
+                _data = _payrollData.Where(o=> o.epf == payItem.epf && o.payCode == payItem.payCode).FirstOrDefault();
+                if(_data != null )
+                {
+                    _data.amount = payItem.rate;
+                    _data.taxContribution = payItem.rate * (decimal)_data.taxConRate;
+                    _data.epfContribution = payItem.rate * (decimal)_data.epfConRate;
+                }
+
+                _context.SaveChanges();
+            });
+
+            //transaction.Commit();
+
+            using var transaction = BeginTransaction();
+
+            Parallel.ForEach(_emp, emp =>
+            {
+                decimal _epfTot = _payrollData.Where(o => o.epf == emp.epf).Sum(w => w.epfContribution);
+                decimal _taxTot = _payrollData.Where(o => o.epf == emp.epf).Sum(w => w.taxContribution);
+            });
+
+            transaction.Commit();
+        }
+
+        public IDbTransaction BeginTransaction()
+        {
+            var transaction = _context.Database.BeginTransaction();
+
+            return transaction.GetDbTransaction();
         }
     }
 }
