961a8b8be91a91f7e249abd90528ba33c9cf052b
pavithra91 <dev.pavithraj@gmail.com>
2023-11-11 15:18:22 +0530

    CPP-38 RFC Connection started

--- PayrollAPI/Controllers/DataTransferController.cs
+++ PayrollAPI/Controllers/DataTransferController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PayrollAPI.Data;
 using PayrollAPI.DataModel;
 using PayrollAPI.Interfaces;
+using SAP.Middleware.Connector;
 
 namespace PayrollAPI.Controllers
 {
@@ -15,6 +17,46 @@ namespace PayrollAPI.Controllers
         {
             _data = data;
         }
+
+        [Route("TestRFC")]
+        [HttpPost]
+        public IActionResult TestRFC()
+        {
+            SAPConnect sap = new SAPConnect();
+            RfcDestinationManager.RegisterDestinationConfiguration(sap);
+            RfcDestination prd = RfcDestinationManager.GetDestination("ZRFC_TEST");
+
+            try
+            {
+                RfcRepository repo = prd.Repository;
+                IRfcFunction companyBapi = repo.CreateFunction("BAPI_COMPANY_GETDETAIL");
+                companyBapi.SetValue("COMPANYID", "001000");
+                companyBapi.Invoke(prd);
+                IRfcStructure detail = companyBapi.GetStructure("COMPANY_DETAIL");
+                String companyName = detail.GetString("NAME1");
+                Console.WriteLine(companyName);
+
+                return Ok();
+            }
+            catch (RfcCommunicationException e)
+            {
+                return BadRequest(e);
+            }
+            catch (RfcLogonException e)
+            {
+                return BadRequest(e);
+            }
+            catch (RfcAbapRuntimeException e)
+            {
+                return BadRequest(e);
+            }
+            catch (RfcAbapBaseException e)
+            {
+                return BadRequest(e);
+            }
+
+        }
+
 
         [Route("ConfirmDataTransfer")]
         [HttpPost]
